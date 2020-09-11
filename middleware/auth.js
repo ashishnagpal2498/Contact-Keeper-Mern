f48b0d695964f5d315d9a7188b34041c06acb9f0
Ashish <ashishnagpal2498@gmail.com>
2020-09-11 23:43:26 +0530

    :up: backend- user model,db and auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+// JWT and Config
+
+module.exports = (req, res, next) => {
+	// Try Catch ->
+
+	// 1. Grab token from header ->
+	// 2. Check if Token valid
+	// 3. decoded -> token.verify()
+	// 4. Return User-> payload ->
+
+
+}
